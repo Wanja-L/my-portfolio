7c02a275a409332b888d647224392f75bdaffb30
Wanja Leuthold <leuthowanj@myvuw.ac.nz>
2018-05-30 18:53:34 +1200

    more setup

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/api/projects', (req, res) => {
     res.json(projectsData);
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.listen(port);
 console.log(`My Portfolio is listening on ${port}`);
 
