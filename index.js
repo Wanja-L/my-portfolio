178f275b9cd905eca0ccabe2fc7b9244a2d4295a
Wanja Leuthold <leuthowanj@myvuw.ac.nz>
2018-05-30 18:24:02 +1200

    setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+console.log(`My Portfolio is listening on ${port}`);
+
+let projectsData = [
+    {
+        name: 'Python playaround on GitHub',
+        html_url: 'http://www.google.com',
+        description: 'I played around with python'
+    }
+];
+
+app.get('/api/projects', (req, res) => {
+    // Return projects as json
+    res.json(projectsData);
+});
