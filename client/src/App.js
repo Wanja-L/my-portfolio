1fd3cfca0f5cc696597cfad18744615e0b1e79b1
Lithowan <wanja.leuthold@gmail.com>
2018-09-03 15:36:30 +1200

    added styling, changed favicon

--- client/src/App.js
+++ client/src/App.js
@@ -22,52 +22,72 @@ class App extends Component {
     const { projects } = this.state;
 
     return (
-      <div className="App" width="500">
-        <img
-          src={portrait}
-          alt="Picture of Wanja"
-          width="500"
-          height="500"
-          style={{ marginTop: 30 }}
-        />
+      <div className="App">
+        <div className="Section">
+          <img
+            src={portrait}
+            style={{ "margin-top": "5%" }}
+            alt="Picture of Wanja"
+            width="70%"
+            height="70%"
+          />
 
-        <h1>Hi, my name is Wanja</h1>
-        <h3>I'm an aspiring developer!</h3>
-        <p>
-          I am a swiss raised kiwi who is passionate about web and tech that
-          changes people's lives.
-        </p>
-        <br />
-        <h4>Contact Info</h4>
-        <p>
-          email:{" "}
-          <a class="u-email" href="mailto:wanja.leuthold@gmail.com">
-            wanja.leuthold@gmail.com
-          </a>
-        </p>
-        <p>mobile: 0210418761</p>
-        <br />
+          <h1>Hi, my name is Wanja</h1>
+          <h2>I'm an aspiring developer!</h2>
 
-        <h4>Here are a few of my projects</h4>
+          <p>
+            I am a swiss raised kiwi who is passionate about web and tech that
+            changes people's lives.
+          </p>
+          <br />
+        </div>
 
-        {projects.length ? (
-          projects
-            .sort((p1, p2) => {
-              return Date.parse(p2.updated_at) - Date.parse(p1.updated_at);
-            })
-            .map((project, index) => (
-              <div key={project.name}>
-                <p>
-                  <b>
-                    <a href={project.html_url}>{project.name}</a>
-                  </b>
-                </p>
-                <p>{project.description}</p>
-              </div>
-            ))
-        ) : (
-          <div>Loading projects...</div>
-        )}
+        <div className="Section">
+          <h3>Contact Info</h3>
+          <p>
+            email:{" "}
+            <a
+              class="u-email"
+              href="mailto:wanja.leuthold@gmail.com"
+              style={{ color: "rgb(50,50,80)" }}
+            >
+              wanja.leuthold@gmail.com
+            </a>
+          </p>
+          <p>mobile: 0210418761</p>
+          <br />
+        </div>
+
+        <div className="Section">
+          <h3>Here are a few of my projects:</h3>
+
+          {projects.length ? (
+            projects
+              .sort((p1, p2) => {
+                return Date.parse(p2.updated_at) - Date.parse(p1.updated_at);
+              })
+              .map((project, index) => (
+                <div className="ProjectCard" key={project.name}>
+                  <p>
+                    <b>
+                      <a
+                        href={project.html_url}
+                        style={{ color: "rgb(60,60,80)" }}
+                      >
+                        {project.name}
+                      </a>
+                    </b>
+                  </p>
+                  <p>{project.description}</p>
+                </div>
+              ))
+          ) : (
+            <div>Loading projects...</div>
+          )}
+        </div>
+        <div className="Footer">
+          <p>This webapp uses React and Node.js, and consumes the github API</p>
+        </div>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fetch = require('node-fetch');
+const path = require("path");
+const fetch = require("node-fetch");
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/projects', (req, res) => {
-    // Return projects as json
-    res.json(projectsData);
+app.get("/api/projects", (req, res) => {
+  // Return projects as json
+  res.json(projectsData);
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port);
@@ -24,25 +24,24 @@ console.log(`My Portfolio is listening on ${port}`);
 
 let projectsData = [];
 
-const githubRepoUrl = 'https://api.github.com/users/Lithowan/repos';
+const githubRepoUrl = "https://api.github.com/users/Lithowan/repos";
 
-function getProjects () {
-    fetch(githubRepoUrl)
-        .then(res => res.json())
-        .then(projects => {
+function getProjects() {
+  fetch(githubRepoUrl)
+    .then(res => res.json())
+    .then(projects => {
+      const gitHubProjects = projects.map(project => {
+        return {
+          name: project.name,
+          html_url: project.html_url,
+          description: project.description,
+          updated_at: project.updated_at
+        };
+      });
 
-            const gitHubProjects = projects.map(project => {
-                return {
-                    name: project.name,
-                    html_url: project.html_url,
-                    description: project.description,
-                    updated_at: project.updated_at
-                }
-            });
-
-            projectsData = projectsData.concat(gitHubProjects);
-            console.log(`Loaded ${projectsData.length} projects`);
-        });
+      projectsData = projectsData.concat(gitHubProjects);
+      console.log(`Loaded ${projectsData.length} projects`);
+    });
 }
 
 getProjects();
